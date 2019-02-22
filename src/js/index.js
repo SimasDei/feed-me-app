18684c8a71936367267cb90aa94fe573bf35b115
SimasDei <simondeiman@gmail.com>
2019-02-22 12:01:39 +0200

    add Food 2 Fork API implementation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,20 @@
-// Global app controller
-import test from './test';
-const x = 23;
-console.log(`${test} an imported module! and const x = ${x}`);
+import axios from 'axios';
+/**
+ * Food 2 Fork API info
+ * API key - 7a6a0bb3542336e661927eee7ef879cb
+ * Get path - https://www.food2fork.com/api/search
+ */
+async function getResults(query) {
+  const apiKey = '7a6a0bb3542336e661927eee7ef879cb';
+  try {
+    const result = await axios(
+      `https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`
+    );
+    const recipes = result.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    throw error;
+  }
+}
+
+getResults('cake');
