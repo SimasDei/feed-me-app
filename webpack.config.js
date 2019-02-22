7fa6228ac002ebc27a87db2df501811afa4fcf00
SimasDei <simondeiman@gmail.com>
2019-02-22 11:12:37 +0200

    babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,16 @@ module.exports = {
       filename: 'index.html',
       template: './src/index.html'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
