e963eb51d1fc598b96d3b27aab94840ebbc1316d
SimasDei <simondeiman@gmail.com>
2019-02-27 15:04:40 +0200

    add Shoppig list Model with create,update,delete methods

---
+++ src/js/models/List.js
@@ -0,0 +1,22 @@
+import uniqid from 'uniqid';
+
+export default class List {
+  constructor() {
+    this.items = [];
+  }
+  addItem(count, unit, ingredient) {
+    const item = {
+      id: uniqid(),
+      count,
+      unit,
+      ingredient
+    };
+  }
+  deleteItem(id) {
+    const index = this.items.findIndex(element => element.id === id);
+    this.items.splice(index, 1);
+  }
+  updateCount(id, newCount) {
+    this.items.find(element => element.id === id).count = newCount;
+  }
+}
